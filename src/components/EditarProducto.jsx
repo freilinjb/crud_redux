5d14a17d8e93bd112fbaccca0c55009b3c44cd47
freilinjb <freilinjb@gmail.com>
2020-05-01 12:50:32 -0400

    creando servidor y diseno de EditarProducto

--- src/components/EditarProducto.jsx
+++ src/components/EditarProducto.jsx
@@ -2,7 +2,39 @@ import React from 'react';
 
 const EditarProducto = () => {
     return ( 
-        <h1>Desde EditarProducto</h1>
+        <div className="row justify-content-center">
+           <div className="col-md-8">
+               <div className="card">
+                   <div className="card-body">
+                       <h2 className="text-center md-4 font-weight-bold">
+                           Editar Producto
+                       </h2>
+                       <form>
+                           <div className="form-group">
+                                <label htmlFor="producto">Nombre del Producto</label>
+                                <input 
+                                    type="text" 
+                                    className="form-control" 
+                                    placeholder="Nombre del Producto"
+                                    name="nombre"
+                                    />
+                           </div>
+                           <div className="form-group">
+                                <label htmlFor="producto">Precio del Producto</label>
+                                <input 
+                                    type="number" 
+                                    className="form-control" 
+                                    placeholder="Precio del Producto"
+                                    name="precio"
+                                    />
+                           </div>
+                           <button type="submit" 
+                                className="btn btn-primary btn-block font-weight w-100">GUARDAR CAMBIOS</button>
+                       </form>
+                   </div>
+               </div>
+           </div>
+       </div>
      );
 }
  
