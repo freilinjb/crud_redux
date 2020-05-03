b8a100a2c6a2ea1a041931fd587f8502af497288
freilinjb <freilinjb@gmail.com>
2020-05-03 09:47:44 -0400

    creacion de un segundo reducer

--- src/reducers/index.jsx
+++ src/reducers/index.jsx
@@ -1,7 +1,9 @@
 import  {combineReducers} from 'redux';
 //combineReducers: combina todos los reducer en uno solo
 import productosReducer from './productosReducer';
+import alertaReducer from './alertaReducer';
 
 export default combineReducers({
-    productos: productosReducer
+    productos: productosReducer,
+    alerta: alertaReducer
 });
